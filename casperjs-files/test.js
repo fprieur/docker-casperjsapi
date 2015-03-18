c7dbc552cee3e02d88e1d385169e7f667518bb6d
Fred Prieur <fprieur@gmail.com>
2015-03-18 16:55:59 -0400

    ajout ficher docker et dockerfile

---
+++ casperjs-files/test.js
@@ -0,0 +1,10 @@
+var casper = require('casper').create();
+casper.start('http://casperjs.org/', function() {
+        this.echo(this.getTitle());
+});
+
+casper.thenOpen('http://phantomjs.org', function() {
+        this.echo(this.getTitle());
+});
+
+casper.run();
